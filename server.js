520e39cf7bd1dbaf061c801c6724bdf2df52f481
Antoine Nosal <antoine.nosal@gmail.com>
2017-02-13 23:30:03 +0100

    ajoute la configuration de mongo

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
+// Dépendances
 var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
+// Démarre express
 var app = express();
 
-// serve static files
+// Connecte et configure mongo
+var db = mongoose.connect('mongodb://localhost/actionsmarket');
+var Schema = mongoose.Schema;
+var StockSchema = new Schema({
+  name: String,
+  symbol: String,
+  price: Number
+});
+var Stock = mongoose.model('Stock', StockSchema);
+
+// Fichiers statiques
 app.use(express.static(__dirname + '/public'));
 app.use('/node_modules',  express.static(__dirname + '/node_modules'));
 
