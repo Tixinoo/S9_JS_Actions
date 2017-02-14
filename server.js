4a7aa875c55d02cee8a7500dafe13d3a7850ca23
Antoine Nosal <antoine.nosal@gmail.com>
2017-02-14 10:59:21 +0100

    ajoute une route pour la recherche

--- server.js
+++ server.js
@@ -2,19 +2,20 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var request = require('request');
 
 // Démarre express
 var app = express();
 
 // Connecte et configure mongo
-var db = mongoose.connect('mongodb://localhost/actionsmarket');
+var Database = mongoose.connect('mongodb://localhost/actionsmarket');
 var Schema = mongoose.Schema;
 var StockSchema = new Schema({
   name: String,
   symbol: String,
   price: Number
 });
-var Stock = mongoose.model('Stock', StockSchema);
+var StockModel = mongoose.model('Stock', StockSchema);
 
 // Fichiers statiques
 app.use(express.static(__dirname + '/public'));
@@ -27,6 +28,21 @@ app.use(bodyParser.json());
 app.route('/hello')
 .get(function (req, res) {
   res.send('Hello World!');
+})
+
+app.route('/search/:symbol')
+.get(function (req, res) {
+  var symbol = req.params.symbol;
+  var query = encodeURIComponent('select * from yahoo.finance.quotes where symbol = "' + symbol + '"');
+  var url = "https://query.yahooapis.com/v1/public/yql?q=env%20'store%3A%2F%2Fdatatables.org%2Falltableswithkeys'%3B%20" + query + "&format=json";
+  request({
+    url: url,
+    json: true
+  }, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.send(body);
+    }
+  })
 })
 
 app.listen('3000');
